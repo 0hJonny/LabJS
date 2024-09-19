610075eeaf8407f0f8a9df5288d3ba50c6ca987c
0hJonny <0hJonny>
2024-09-19 23:43:33 +0500

    Add new page

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # ToDo List
-================
 
 [FIGMA](https://www.figma.com/design/PF0wTLBBpJzLUewuYkqLky/%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D1%8B_figma?node-id=21188-1075&node-type=frame&t=aZme2S4LP7loxQ7R-0)
 
--- app.js
+++ app.js
@@ -1,1 +1,92 @@
-console.log("Hello World!")
+const todoList = document.querySelector('.todo-list');
+const buttonAdd = document.querySelector('.button-add');
+const emptyState = document.querySelector('.empty-state');
+const todoCreateForm = document.querySelector('.todo-create-form');
+
+function createTodoItem(todoText) {
+    if (!todoText) return;
+
+    var todoItem = document.createElement('div');
+    todoItem.classList.add('todo-item');
+    todoItem.innerHTML = `
+        <button type="button" class="button-select">
+            <img src="images/Check1.svg" alt="Выбрать">
+        </button>
+        <div class="todo-item__text">${todoText}</div>
+        <button type="button" class="button-delete">
+            <img src="images/Cross.svg" alt="Удалить задачу">
+        </button>
+    `;
+    todoList.append(todoItem);
+
+    checkViewTodoList()
+    saveToLocalStorage();
+}
+
+todoList.addEventListener('click', (event) => {
+    var target = event.target;
+
+    var selectButton = target.closest('.button-select');
+    var deleteButton = target.closest('.button-delete');
+
+    if (selectButton) {
+        let todoItem = selectButton.closest('.todo-item');
+        todoItem.classList.toggle('selected');
+    }
+
+    if (deleteButton) {
+        let todoItem = deleteButton.closest('.todo-item');
+        todoItem.remove();
+        checkViewTodoList();
+        saveToLocalStorage();
+    }
+});
+
+todoCreateForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    var inputTaskArea = todoCreateForm.querySelector('#input-task-area');
+    var todoText = inputTaskArea.value.trim();
+
+    if (todoText) {
+        createTodoItem(todoText);
+        inputTaskArea.value = '';
+    }
+
+    todoCreateForm.style.display = 'none';
+});
+
+buttonAdd.addEventListener('click', () => {
+    todoList.style.display = 'none';
+    emptyState.style.display = 'none';
+    todoCreateForm.style.display = 'block';
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    todoCreateForm.querySelector('#input-task-area').focus();
+});
+
+function saveToLocalStorage() {
+    var todoItems = Array.from(todoList.querySelectorAll('.todo-item__text')).map(todoItem => todoItem.textContent);
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+}
+
+function loadFromLocalStorage() {
+    var todoItemsString = localStorage.getItem('todoItems');
+    if (todoItemsString) {
+        let todoItems = JSON.parse(todoItemsString);
+        todoItems.forEach(todoText => {
+            createTodoItem(todoText);
+        });
+    }
+}
+
+function checkViewTodoList() {
+    if (todoList.children.length === 0) {
+        emptyState.style.display = 'flex';
+        todoList.style.display = 'none';
+    } else {
+        emptyState.style.display = 'none';
+        todoList.style.display = 'flex';
+    }
+}
+
+loadFromLocalStorage();
+checkViewTodoList();
